722442b1b50091cf317372d7c2e82201bda08171
Andrey Vernov <a.vernov16@gmail.com>
2021-05-16 15:11:14 +0500

    step 8

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -4,12 +4,14 @@ import game from '../index.js';
 export const gameRules = 'What number is missing in the progression?';
 export const gameData = () => {
   const step = _.random(1, 10);
-  const num1 = _.random(1, 100);
-  const num2 = _.random(1, 10);
+  let num1 = _.random(1, 100);
+  const length = _.random(5, 10);
+  const num2 = _.random(0, length);
   const result = [];
   let rightAnswer = 0;
-  for (let i = 0; i < 10; i += 1) {
-    result.push(num1 + step);
+  for (let i = 0; i < length; i += 1) {
+    num1 += step;
+    result.push(num1);
   }
   rightAnswer = result[num2];
   result[num2] = '..';
